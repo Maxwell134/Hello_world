0d1692df45271cd3116d1a011037b2de7ee12a15
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-03 22:23:36 +0530

    Create dock.py

---
+++ dock.py
@@ -0,0 +1,21 @@
+import os
+
+def docker_login(username, password):
+
+    if username and password:
+        print(f"{username}exist and login sucessfully")
+
+    else:
+        print("User doesnot exist")
+
+
+def main():
+    username = os.environ.get("username")
+    password = os.environ.get("password")
+
+    docker_login(username, password)
+
+if __name__ == "__main__":
+    main()
+
+
