81590e76ca3a73db26f2e6074adc0b46182bfea5
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-04 18:39:13 +0530

    re

--- docker_login.py
+++ docker_login.py
@@ -1,20 +1,31 @@
-# docker_login.py
-import subprocess
-import sys
 import os
+import subprocess
 
-def docker_login(username, password, registry):
-    try:
-        login_cmd = f"docker login -u {username} --password-stdin {registry}"
-        subprocess.run(login_cmd, input=password.encode('utf-8'), shell=True, check=True)
-        print("Docker login successful.")
-    except subprocess.CalledProcessError as e:
-        print(f"Docker login failed. Error: {e}")
-        sys.exit(1)
+def check_and_remove_docker_image(image_name, username, password):
+    # Login to Docker Hub
+    docker_login_cmd = f'echo "{password}" | docker login -u "{username}" --password-stdin'
+    subprocess.run(docker_login_cmd, shell=True, check=True)
+
+    # Check if the image exists
+    docker_pull_cmd = f'docker pull {image_name}'
+    pull_result = subprocess.run(docker_pull_cmd, shell=True, stderr=subprocess.PIPE)
+
+    if "Image is up to date" in pull_result.stderr.decode():
+        print(f"Image {image_name} already exists on Docker Hub. Removing it...")
+        docker_rmi_cmd = f'docker rmi {image_name}'
+        subprocess.run(docker_rmi_cmd, shell=True, check=True)
+        print("Removed existing image.")
+        return True
+    else:
+        print(f"Image {image_name} doesn't exist on Docker Hub.")
+        return False
 
 if __name__ == "__main__":
-    docker_username = os.getenv("DOCKER_USERNAME")
-    docker_password = os.getenv("DOCKER_PASSWORD")
-    docker_registry = os.getenv("DOCKER_REGISTRY", "docker.io")  # Default to Docker Hub
+    image_name = os.environ.get('INPUT_IMAGE')
+    docker_username = os.environ.get('DOCKER_USERNAME')
+    docker_password = os.environ.get('DOCKER_PASSWORD')
 
-    docker_login(docker_username, docker_password, docker_registry)
+    image_exists = check_and_remove_docker_image(image_name, docker_username, docker_password)
+
+    # Output result for GitHub Actions
+    print(f"::set-output name=image_exists::{str(image_exists).lower()}")
