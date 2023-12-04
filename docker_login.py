24314836ff574fa6a0b57772b0162e1b33994dd6
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-05 01:30:36 +0530

    sx

--- docker_login.py
+++ docker_login.py
@@ -1,22 +1,29 @@
 import os
 import subprocess
+import requests
 
 def check_docker_image_exists(image_name, username, password):
     # Login to Docker Hub
     docker_login_cmd = f'docker login -u {username} -p {password}'
-
+    api_url = f'https://hub.docker.com/v2/repositories/{username}/{image_name}/tags'
+    
     subprocess.run(docker_login_cmd, shell=True, check=True)
+
+    response = requests.get(api_url)
 
     # Check if the image exists
     docker_pull_cmd = f'docker pull {image_name}'
     pull_result = subprocess.run(docker_pull_cmd, shell=True, stderr=subprocess.PIPE)
 
-    if "Image is up to date" in pull_result.stderr.decode():
+    if response.status_code == 200:
         print(f"Image {image_name} exists on Docker Hub.")
-        return True
-    else:
+        return "exist"
+    elif response.status_code == 404:
         print(f"Image {image_name} does not exist on Docker Hub.")
-        return False
+        return "none"
+    else:
+        print(f"Failed to check the status of the image on Docker Hub. Status code: {response.status_code}")
+        return "error"
 
 if __name__ == "__main__":
     image_name = os.environ.get('INPUT_IMAGE')
