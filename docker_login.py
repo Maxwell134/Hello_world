9972888245fb7327e90dae5ff55c51002a1e1ff4
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-05 00:15:55 +0530

    p

--- docker_login.py
+++ docker_login.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-def check_and_remove_docker_image(image_name, username, password):
+def check_docker_image_exists(image_name, username, password):
     # Login to Docker Hub
     docker_login_cmd = f'echo "{password}" | docker login -u "{username}" --password-stdin'
     subprocess.run(docker_login_cmd, shell=True, check=True)
@@ -11,13 +11,10 @@ def check_and_remove_docker_image(image_name, username, password):
     pull_result = subprocess.run(docker_pull_cmd, shell=True, stderr=subprocess.PIPE)
 
     if "Image is up to date" in pull_result.stderr.decode():
-        print(f"Image {image_name} already exists on Docker Hub. Removing it...")
-        docker_rmi_cmd = f'docker rmi {image_name}'
-        subprocess.run(docker_rmi_cmd, shell=True, check=True)
-        print("Removed existing image.")
+        print(f"Image {image_name} exists on Docker Hub.")
         return True
     else:
-        print(f"Image {image_name} doesn't exist on Docker Hub.")
+        print(f"Image {image_name} does not exist on Docker Hub.")
         return False
 
 if __name__ == "__main__":
@@ -25,7 +22,7 @@ if __name__ == "__main__":
     docker_username = os.environ.get('DOCKER_USERNAME')
     docker_password = os.environ.get('DOCKER_PASSWORD')
 
-    image_exists = check_and_remove_docker_image(image_name, docker_username, docker_password)
+    image_exists = check_docker_image_exists(image_name, docker_username, docker_password)
 
     # Output result for GitHub Actions
     print(f"::set-output name=image_exists::{str(image_exists).lower()}")
