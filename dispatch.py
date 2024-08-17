6febde5960389bcbcbcd3521cdda64460444e399
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-17 16:34:01 +0530

    Update dispatch.py

--- dispatch.py
+++ dispatch.py
@@ -7,20 +7,21 @@ def run():
     event_type = os.getenv('INPUT_EVENT_TYPE')
     token = os.getenv('INPUT_TOKEN')
 
-    url = f'https://api.github.com/repos/{repo_name}/dispatches'
+    if not repo_name or not event_type or not token:
+        raise ValueError("One or more required environment variables are not set.")
 
+    url = f'https://api.github.com/repos/{repo_name}/dispatches'
     headers = {
         'Authorization': f'Bearer {token}',
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28'
+        'Accept': 'application/vnd.github+json'
     }
-
     data = {
-        'event_type': event_type,
-        'client_payload': {}
+        'event_type': event_type
     }
-
     response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
 
     if response.status_code == 204:
         print(f"Dispatched event {event_type} to {repo_name}")
